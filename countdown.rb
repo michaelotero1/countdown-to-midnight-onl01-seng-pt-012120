9c14f2b2f4d7fa00ccfb61b9a8c0056625630b6a
michael otero <michaelotero1118@gmail.com>
2020-01-31 18:24:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,10 @@ def countdown(seconds)
    "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(seconds)
+  while seconds > 0
+   puts "#{seconds} SECOND(S)!"
+    seconds -= 1  
+  end
+   "HAPPY NEW YEAR!"
+end
